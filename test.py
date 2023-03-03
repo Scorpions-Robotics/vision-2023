e8c04f931d2d6f1a89051f407f3707a59bbe6cae
egeakman <me@egeakman.dev>
2023-03-03 14:32:00 +0300

    test rtp

---
+++ test.py
@@ -0,0 +1,34 @@
+import sys
+import cv2
+
+def read_cam():
+    cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! qtdemux ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, width=640, height=480 format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink")
+
+    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print('Src opened, %dx%d @ %d fps' % (w, h, fps))
+
+    gst_out = "appsrc ! video/x-raw, format=BGR ! queue ! videoconvert ! video/x-raw,format=BGRx ! nvvidconv ! nvv4l2h264enc ! h264parse ! rtph264pay pt=96 config-interval=1 ! udpsink host=10.76.72.10 port=5807"
+    out = cv2.VideoWriter(gst_out, cv2.CAP_GSTREAMER, 0, float(fps), (int(w), int(h)))
+    if not out.isOpened():
+        print("Failed to open output")
+        exit()
+
+    if cap.isOpened():
+        while True:
+            ret_val, img = cap.read()
+            if not ret_val:
+                break
+            out.write(img)
+            cv2.waitKey(1)
+    else:
+     print("pipeline open failed")
+
+    print("successfully exit")
+    cap.release()
+    out.release()
+
+
+if __name__ == '__main__':
+    read_cam()
